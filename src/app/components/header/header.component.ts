70e2b2bd3dc618ebf1658f70c2cf57b6d7dfeab9
Karim Chakib <k.chakib12@gmail.com>
2021-12-05 20:31:41 +0100

    Limit component to route

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = true;
   subscription: Subscription = new Subscription();
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = uiService.onToggle().subscribe(value => this.showAddTask = value);
    }
 
@@ -23,5 +24,9 @@ export class HeaderComponent implements OnInit {
   toggleAddTask(){
     this.uiService.toggleAddTask();
     console.log('toggle adding task!')
+  }
+
+  hasRoute(route: string): boolean{
+   return this.router.url == route; 
   }
 }
